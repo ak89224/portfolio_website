59b29ab74636bd4fc17533d5b6caf460b90a635b
ak89224 <52851630+ak89224@users.noreply.github.com>
2019-07-17 04:16:51 +0530

    Blog app backend up and running

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class blog(models.Model):
+    blog_title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField(null=True, default=timezone.now())
+    body = models.TextField(default='None')
+    thumbnail = models.ImageField(upload_to='images/')
