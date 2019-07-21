9cd4b509a9e4a781ce5b7be5d55df49b2a494c96
ak89224 <52851630+ak89224@users.noreply.github.com>
2019-07-22 01:32:05 +0530

    Added Bootstrap for Blog Page. Up and working

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,4 @@
-"""Portfolio_Website URL Configuration
+"""blog URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -13,15 +13,11 @@ Including another URLconfJobsConfig
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
-import jobs.views
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',jobs.views.homepage,name='home'),
-    path('blog/', include('blog.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('',views.allblogs,name='allblogs'),
+
+]
 
